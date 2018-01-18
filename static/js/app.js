4f77aa30214eeb8ab6f02e8b9f8f6d47f18110eb
Unknown <step.earthz@gmail.com>
2018-01-18 12:16:39 +0700

    test naja

---
+++ static/js/app.js
@@ -0,0 +1,28 @@
+$(function() {
+    $('#submit').click(function() {
+        event.preventDefault();
+        var form_data = new FormData($('#uploadform')[0]);
+        $.ajax({
+            type: 'POST',
+            url: '/uploadajax',
+            data: form_data,
+            contentType: false,
+            processData: false,
+            dataType: 'json'
+        }).done(function(data, textStatus, jqXHR){
+            console.log('data',data);
+            console.log(textStatus);
+            console.log(jqXHR);
+            console.log('Success!');
+            $("#resultFilename").text(data['name']);
+            $("#resultFilesize").text(data['size']);
+            $("#resImg").attr({
+              src: "upload/test.jpg",
+              title: "jQuery",
+              alt: "jQuery Logo"
+            });
+        }).fail(function(data){
+            alert('error!');
+        });
+    });
+});
